e641669944037cb280eba8a5a5f3370047dbf8be
t2ga <tltsnsub@gmail.com>
2021-05-30 14:37:45 +0300

    [add] Step 7: Nav 2 Routes with Mandatory Param

---
+++ webapp/controller/employee/Employee.controller.js
@@ -0,0 +1,52 @@
+sap.ui.define([
+	"com/zwork/znavigation/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+	return BaseController.extend("com.zwork.znavigation.controller.employee.Employee", {
+		onInit: function () {
+			var oRouter = this.getRouter();
+			oRouter.getRoute("employee").attachMatched(this._onRouteMatched, this);
+			// Hint: we don't want to do it this way
+			/*  Note
+                Instead of calling attachMatched(…) on a route we could also call 
+                attachRouteMatched(…) directly on the router. 
+                However, the event for the latter is fired for every matched event of any route 
+                in the whole app. We don’t use the latter because we would have to implement 
+                an additional check for making sure that current route is the route that has been matched. 
+                We want to avoid this extra overhead and register on the route instead.
+            /*
+			oRouter.attachRouteMatched(function (oEvent){
+				var sRouteName, oArgs, oView;
+				sRouteName = oEvent.getParameter("name");
+				if (sRouteName === "employee"){
+					this._onRouteMatched(oEvent);
+				}
+			}, this);
+			*/
+		},
+		_onRouteMatched : function (oEvent) {
+			var oArgs, oView;
+			oArgs = oEvent.getParameter("arguments");
+			oView = this.getView();
+
+			oView.bindElement({
+				path : "/Employees(" + oArgs.employeeId + ")",
+				events : {
+					change: this._onBindingChange.bind(this),
+					dataRequested: function (oEvent) {
+						oView.setBusy(true);
+					},
+					dataReceived: function (oEvent) {
+						oView.setBusy(false);
+					}
+				}
+			});
+		},
+		_onBindingChange : function (oEvent) {
+			// No data for the binding
+			if (!this.getView().getBindingContext()) {
+				this.getRouter().getTargets().display("notFound");
+			}
+		}
+	});
+});
--- webapp/controller/employee/EmployeeList.controller.js
+++ webapp/controller/employee/EmployeeList.controller.js
@@ -3,5 +3,13 @@ sap.ui.define([
 ], function (BaseController) {
 	"use strict";
 	return BaseController.extend("com.zwork.znavigation.controller.employee.EmployeeList", {
+        onListItemPressed: function (oEvent) {
+            var oItem, oCtx;
+            oItem = oEvent.getSource();
+            oCtx = oItem.getBindingContext();
+            this.getRouter().navTo("employee", {
+                employeeId: oCtx.getProperty("EmployeeID") //Id from data model
+            });
+        }
 	});
 });
